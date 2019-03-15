ea4616c959288312e70ddf9880fa5b8c173d9176
sanadimp <sanadimp@DIN8004108.corp.capgemini.com>
2019-03-15 08:44:25 +0530

    commit3

---
+++ Imdb_SpringMvc/src/main/resources/com/cg/HelloController.java
@@ -0,0 +1,16 @@
+package com.cg;
+
+ 
+import org.springframework.stereotype.Controller;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+@Controller  
+
+	public class HelloController {  
+		@RequestMapping("/Movie1")  
+		public ModelAndView Movie1()
+		  
+		    {  
+		      ModelAndView mav=new ModelAndView("Movie1");  
+		      return mav;
+		    }     
+		}  
