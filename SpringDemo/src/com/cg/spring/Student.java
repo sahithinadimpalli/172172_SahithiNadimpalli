2cd1e0f1ed68b8843cc40232a376f56dd88fb10a
sanadimp <sanadimp@DIN8004108.corp.capgemini.com>
2019-03-15 09:06:45 +0530

    commit14

---
+++ SpringDemo/src/com/cg/spring/Student.java
@@ -0,0 +1,19 @@
+package com.cg.spring;
+
+public class Student  String name;
+
+{
+	String name;  
+
+public String getName() {  
+  return getName();  
+}  
+
+public void setName(String name) {  
+  this.name = name;  
+}  
+
+public void displayInfo(){  
+  System.out.println("Hello: "+name);  
+}  
+}  
